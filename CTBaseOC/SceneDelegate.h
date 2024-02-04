d6bf6b6fe369249d522efddf5b919785a38ae437
curtis <huangguocai9533@xwtec.cn>
2024-02-04 22:34:48 +0800

    init

---
+++ CTBaseOC/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  CTBaseOC
+//
+//  Created by Curtis on 2024/2/4.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ CTBaseOC/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  CTBaseOC
+//
+//  Created by Curtis on 2024/2/4.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
